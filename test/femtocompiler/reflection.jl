3a55df10e5ebf7ed70028fd959f7087c6c1e2f23
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-22 08:44:53 +0900

    test Base.invoke_interp_compiler

---
+++ test/femtocompiler/reflection.jl
@@ -0,0 +1,22 @@
+module test_femtocompiler_reflection
+
+using Test
+using FemtoCompiler: FemtoInterpreter
+using Core: Compiler as CC
+using .CC: typename, MethodInstance, InferenceResult, typeinf_lattice, Const, InferenceState, CodeInfo, CurrentState
+
+f = +
+types = (Int, Int)
+
+m = methods(f, types)
+mi::MethodInstance = m[1].specializations[1]
+interp = FemtoInterpreter()
+result = InferenceResult(mi, typeinf_lattice(interp))
+@test result.argtypes == [Const(+), Int, Int]
+frame = InferenceState(result, #=cache_mode=#:no, interp)
+@test frame.src isa CodeInfo
+nextresult1 = CurrentState()
+nextresult2 = Base.invoke_interp_compiler(interp, :typeinf_local, interp, frame, nextresult1)
+@test nextresult2 isa CurrentState
+
+end # module test_femtocompiler_reflection
