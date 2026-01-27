d235d633bc3d62b6d05e6635d4080dd7a7433116
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-27 20:44:15 +0900

    precompile

--- ext/precompile.jl
+++ ext/precompile.jl
@@ -1,12 +1,20 @@
 # module CompilerExt
 
+precompile(Base.invoke_interp_compiler, (FemtoInterpreter, Symbol, FemtoInterpreter, Vararg{Any}))
+precompile(CC._findall_matches, (FemtoInterpreter, Any))
+precompile(CC.typeinf_code, (FemtoInterpreter, MethodMatch, Bool))
+precompile(CC.typeinf_type, (FemtoInterpreter, MethodInstance))
+
+# ext/abstractinterpretation.jl
 precompile(CC.typeinf_local, (FemtoInterpreter, InferenceState, CurrentState))
 
+# ext/typeinfer.jl
 precompile(CC.typeinf_edge, (FemtoInterpreter, Method, Any, SimpleVector, AbsIntState, Bool, Bool))
 precompile(CC.typeinf_frame, (FemtoInterpreter, MethodMatch, Bool))
 precompile(CC.typeinf_frame, (FemtoInterpreter, Method, Any, SimpleVector, Bool))
 precompile(CC.typeinf_frame, (FemtoInterpreter, MethodInstance, Bool))
 
+# ext/newinterp.jl
 precompile(CC.InferenceParams, (FemtoInterpreter,))
 precompile(CC.OptimizationParams, (FemtoInterpreter,))
 precompile(CC.get_inference_world, (FemtoInterpreter,))
