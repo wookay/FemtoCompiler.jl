a3a9fc5d8326719c9839c0b91c94eb1c66f31aab
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-06 09:53:11 +0900

    precompile `CC.typeinf_local(::CC.NativeInterpreter, ::CC.InferenceState, ::CC.CurrentState)`

--- ext/precompile.jl
+++ ext/precompile.jl
@@ -38,8 +38,9 @@ end
 precompile(CC.statement_costs!, (FemtoInterpreter, Array{Int, 1}, Array{Any, 1}, Core.CodeInfo, Core.MethodMatch))
 
 ### CC.NativeInterpreter
-precompile(Tuple{typeof(CC._infer_effects), CC.NativeInterpreter, Any, Bool})
 precompile(Tuple{typeof(CC.typeinf_code), CC.NativeInterpreter, Core.MethodMatch, Bool})
+precompile(Tuple{typeof(CC.typeinf_local), CC.NativeInterpreter, CC.InferenceState, CC.CurrentState})
 precompile(Tuple{typeof(CC.typeinf_type), CC.NativeInterpreter, Core.MethodMatch})
+precompile(Tuple{typeof(CC._infer_effects), CC.NativeInterpreter, Any, Bool})
 
 # module CompilerExt
--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,6 @@
 # module FemtoCompiler
 
+# FemtoCompiler.OverlayPlus
 precompile(Tuple{typeof(Base.Compiler._findall_matches), FemtoCompiler.FemtoInterpreter, Type{Tuple{typeof(OverlayPlus.overlay_plus), Int, Int}}})
 
 # module FemtoCompiler
