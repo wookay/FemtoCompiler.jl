70830de14f8effb9a2f96a010155ae075c3bf44b
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-07 09:01:35 +0900

    precompile FemtoCompiler.code_typed1

--- ext/precompile.jl
+++ ext/precompile.jl
@@ -23,24 +23,39 @@ precompile(CC.typeinf_frame, (FemtoInterpreter, MethodMatch, Bool))
 precompile(CC.typeinf_frame, (FemtoInterpreter, Method, Any, SimpleVector, Bool))
 precompile(CC.typeinf_frame, (FemtoInterpreter, MethodInstance, Bool))
 
+# FemtoInterpreter - Core
+#=  186.1 ms =# precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:interp,), Tuple{FemtoCompiler.FemtoInterpreter}}, typeof(Base.print_statement_costs), Base.GenericIOBuffer{Memory{UInt8}}, Type})
+#=    6.4 ms =# precompile(Tuple{Type{FemtoCompiler.FemtoInterpreter}})
+
 # FemtoInterpreter - Base
 precompile(Base.invoke_interp_compiler, (FemtoInterpreter, Symbol, FemtoInterpreter, Vararg{Any}))
 
 # FemtoInterpreter - CC
-precompile(CC.typeinf_code, (FemtoInterpreter, MethodMatch, Bool))
+#=    6.7 ms =# precompile(Tuple{typeof(Base.Compiler._findall_matches), FemtoCompiler.FemtoInterpreter, Any}) # recompile
+#= 2949.7 ms =# precompile(Tuple{typeof(Base.Compiler.typeinf_code), FemtoCompiler.FemtoInterpreter, Core.MethodMatch, Bool})
+#=   71.7 ms =# precompile(Tuple{typeof(Base.Compiler.finish_nocycle), FemtoCompiler.FemtoInterpreter, Base.Compiler.InferenceState, UInt64})
 precompile(CC.typeinf_type, (FemtoInterpreter, MethodInstance))
 precompile(CC.doworkloop, (FemtoInterpreter, CC.InferenceState))
-precompile(CC.finish_nocycle, (FemtoInterpreter, CC.InferenceState, UInt64))
 if VERSION >= v"1.13.0-DEV.483" # julia commit 3864b18af6
 precompile(CC.compute_inlining_cost, (FemtoInterpreter, CC.InferenceResult, CC.OptimizationResult))
-precompile(Tuple{typeof(CC.compute_inlining_cost), CC.NativeInterpreter, CC.InferenceResult, CC.OptimizationResult})
 end
 precompile(CC.statement_costs!, (FemtoInterpreter, Array{Int, 1}, Array{Any, 1}, Core.CodeInfo, Core.MethodMatch))
 
 ### CC.NativeInterpreter
-precompile(Tuple{typeof(CC.typeinf_code), CC.NativeInterpreter, Core.MethodMatch, Bool})
 precompile(Tuple{typeof(CC.typeinf_local), CC.NativeInterpreter, CC.InferenceState, CC.CurrentState})
 precompile(Tuple{typeof(CC.typeinf_type), CC.NativeInterpreter, Core.MethodMatch})
 precompile(Tuple{typeof(CC._infer_effects), CC.NativeInterpreter, Any, Bool})
+#=   44.2 ms =# precompile(Tuple{typeof(Base.Compiler.typeinf_code), Base.Compiler.NativeInterpreter, Core.MethodMatch, Bool})
+#=    6.2 ms =# precompile(Tuple{typeof(Base.Compiler._findall_matches), Base.Compiler.NativeInterpreter, Any}) # recompile
+if VERSION >= v"1.13.0-DEV.483" # julia commit 3864b18af6
+#=    5.0 ms =# precompile(Tuple{typeof(Base.Compiler.compute_inlining_cost), Base.Compiler.NativeInterpreter, Base.Compiler.InferenceResult, Base.Compiler.OptimizationResult})
+end
+#=   18.0 ms =# precompile(Tuple{typeof(Base.Compiler.statement_costs!), Base.Compiler.NativeInterpreter, Array{Int64, 1}, Array{Any, 1}, Core.CodeInfo, Core.MethodMatch})
+#=    4.9 ms =# precompile(Tuple{typeof(Base.invoke_interp_compiler), Nothing, Symbol, Base.Compiler.NativeInterpreter, Vararg{Any}})
+#=    4.9 ms =# precompile(Tuple{typeof(Base.invoke_default_compiler), Symbol, Base.Compiler.NativeInterpreter, Vararg{Any}})
+
+# Base
+#=    4.8 ms =# precompile(Tuple{typeof(Base.findall), Type, Base.Compiler.CachedMethodTable{Base.Compiler.InternalMethodTable}})
+#=   28.6 ms =# precompile(Tuple{typeof(Base.print_statement_costs), Base.GenericIOBuffer{Memory{UInt8}}, Type})
 
 # module CompilerExt
--- src/precompile.jl
+++ src/precompile.jl
@@ -1,6 +1,10 @@
 # module FemtoCompiler
 
-# FemtoCompiler.OverlayPlus
+# irutils.jl - FemtoCompiler.code_typed1
+precompile(Tuple{typeof(FemtoCompiler.code_typed1), Function, Tuple{typeof(DataType), typeof(DataType)}})
+precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:interp,), Tuple{FemtoCompiler.FemtoInterpreter}}, typeof(FemtoCompiler.code_typed1), Function, Tuple{typeof(DataType), typeof(DataType)}})
+
+# overlay_plus.jl - FemtoCompiler.OverlayPlus
 precompile(Tuple{typeof(Base.Compiler._findall_matches), FemtoCompiler.FemtoInterpreter, Type{Tuple{typeof(OverlayPlus.overlay_plus), Int, Int}}})
 
 # module FemtoCompiler
