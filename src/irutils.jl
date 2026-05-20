4685adc5859fa0215698d9465f5a8121b1b6226e
WooKyoung Noh <wookay.noh@gmail.com>
2026-05-20 10:59:07 +0900

    `code_statement_costs` (which is taken from the function `Base.print_statement_costs`)

--- src/irutils.jl
+++ src/irutils.jl
@@ -6,4 +6,33 @@ function code_typed1(f::Function, types::Tuple; interp::Union{Nothing,AbstractIn
     first(only(Base.code_typed(f, types; interp)))
 end
 
+# from julia/base/reflection.jl
+# function print_statement_costs(io::IO, @nospecialize(tt::Type);
+#                                world::UInt=get_world_counter(),
+#                                interp=nothing)
+using Base: invoke_default_compiler, to_tuple_type, invoke_interp_compiler, raise_match_failure
+function code_statement_costs(f::Function, types::Tuple; interp::Union{Nothing,AbstractInterpreter} = nothing)::Union{Nothing, Int}
+    tt = Base.signature_type(f, types)
+    world = Base.get_world_counter()
+    passed_interp = interp
+    interp = passed_interp === nothing ? invoke_default_compiler(:_default_interp, world) : interp
+    tt = to_tuple_type(tt)
+    world == typemax(UInt) && error("code reflection cannot be used from generated functions")
+    matches = invoke_interp_compiler(passed_interp, :_findall_matches, interp, tt)
+    matches === nothing && raise_match_failure(:code_statement_costs, tt)
+    cst = Int[]
+    for match in matches.matches
+        match = match::Core.MethodMatch
+        code = invoke_interp_compiler(passed_interp, :typeinf_code, interp, match, true)
+        if code === nothing
+        else
+            empty!(cst)
+            resize!(cst, length(code.code))
+            maxcost = invoke_interp_compiler(passed_interp, :statement_costs!, interp, cst, code.code, code, match)
+            return maxcost
+        end
+    end
+    return nothing
+end
+
 # module FemtoCompiler
--- test/femtocompiler/reflection.jl
+++ test/femtocompiler/reflection.jl
@@ -23,15 +23,14 @@ nextresult2 = Base.invoke_interp_compiler(interp, :typeinf_local, interp, frame,
 @test nextresult2 isa CurrentState
 
 tt = Base.signature_type(f, types)
-#= Base.print_statement_costs(stdout, tt)
-+(x::T, y::T) where T<:Union{Int128, Int16, Int32, Int64, Int8, UInt128, UInt16, UInt32, UInt64, UInt8} @ Base int.jl:87
+# Base.print_statement_costs(stdout, tt)
+#=
++(x::Int64, y::Int64) @ Base essentials.jl:1226
  1 1 ─ %1 = intrinsic Base.add_int(_2, _3)::Int64
  0 └──      return %1
 =#
-buf1 = IOBuffer()
-Base.print_statement_costs(buf1, tt; interp)
-buf2 = IOBuffer()
-Base.print_statement_costs(buf2, tt)
-@test take!(buf1) == take!(buf2)
+
+maxcost = FemtoCompiler.code_statement_costs(f, types; interp)
+@test maxcost == 1
 
 end # module test_femtocompiler_reflection
